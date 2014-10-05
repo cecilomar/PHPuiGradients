256806d090cece39dc7e62fb102877e18fb8e0bc
Cecil O. Almonte <info@cecilomar.com>
2014-10-05 17:31:27 -0500

    Lots of changes
    Added variables to the function().
    Removed the CSS header.
    Added To Do comments.
    Added colour PHP output.

--- phpuigradients.php
+++ phpuigradients.php
@@ -1,30 +1,45 @@
 <?php
 
 // Create the function
-function PHPuiGradients(){
-	// Code will be here
+function PHPuiGradients($selector='.PHPuiGradients', $css=true){
 
-	// Header so is recognized as a CSS file
-	header("Content-type: text/css");
+	// If you want to print the CSS code
+	if($css){
 
-	// Load json into Array
-	$uiGradients = json_decode(file_get_contents('./lib/uiGradients/gradients.json'), true);
+		// Load and parse jSon into Array()
+		$uiGradients = json_decode(file_get_contents('./lib/uiGradients/gradients.json'), true);
 
-	// Get a random color combination
-	$randGradient = $uiGradients[array_rand($uiGradients)];
+		// Get a random color combination
+		$randGradient = $uiGradients[array_rand($uiGradients)];
 
-	// Generate CSS Code
-	?>
-	.uiGradients {
-		background: -webkit-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* Chrome 10+, Saf5.1+ */
-		background:    -moz-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* FF3.6+ */
-		background:     -ms-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* IE10 */
-		background:      -o-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* Opera 11.10+ */
-		background:         linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* W3C */
-	}
+		// To do: Randomize direction of colors. Eg: Left to Right, Right to left.
+
+		// Load CSS Template
+		
+		// To do: This is probably going to be the hardest part to do. Is really not that bad but I'll do it at the end.
+		// For now is only going to generate a horizontal gradient.
+
+		// Generate CSS Code
+		// This part is to be removed once the template part is ready.
+
+		?><?=$selector;?> {
+	background: -webkit-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* Chrome 10+, Saf5.1+ */
+	background:    -moz-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* FF3.6+ */
+	background:     -ms-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* IE10 */
+	background:      -o-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* Opera 11.10+ */
+	background:         linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* W3C */
+}
 
 <?php
-// This is here to end the PHP function
+	}
+
+	// Return the colour variables
+	return array(
+		'colour1' => $randGradient['colour1'],
+		'colour2' => $randGradient['colour2']
+		);
+
+// End of the PHP function
 }
 
 ?>
