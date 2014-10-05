6770149de0972448c5e8781faaf3467668bc8898
Cecil O. Almonte <info@cecilomar.com>
2014-10-05 13:33:15 -0500

    Adde rough code
    This is the code that I made before I started this repository. Now, I
    can start working in the real code.

--- phpuigradients.php
+++ phpuigradients.php
@@ -3,6 +3,28 @@
 // Create the function
 function PHPuiGradients(){
 	// Code will be here
+
+	// Header so is recognized as a CSS file
+	header("Content-type: text/css");
+
+	// Load json into Array
+	$uiGradients = json_decode(file_get_contents('./lib/uiGradients/gradients.json'), true);
+
+	// Get a random color combination
+	$randGradient = $uiGradients[array_rand($uiGradients)];
+
+	// Generate CSS Code
+	?>
+	.uiGradients {
+		background: -webkit-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* Chrome 10+, Saf5.1+ */
+		background:    -moz-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* FF3.6+ */
+		background:     -ms-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* IE10 */
+		background:      -o-linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* Opera 11.10+ */
+		background:         linear-gradient(90deg, <?=$randGradient['colour1'];?> 10%, <?=$randGradient['colour2'];?> 90%); /* W3C */
+	}
+
+<?php
+// This is here to end the PHP function
 }
 
 ?>
